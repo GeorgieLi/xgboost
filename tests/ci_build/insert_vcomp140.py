2fa03ffa8d3797b45124fe558128ebd5695e8c86
Philip Cho <chohyu01@cs.washington.edu>
2019-05-14 15:04:53 -0700

    Yet another fix

--- tests/ci_build/insert_vcomp140.py
+++ tests/ci_build/insert_vcomp140.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import zipfile
+import glob
 
 if len(sys.argv) != 2:
     print('Usage: {} [wheel]'.format(sys.argv[0]))
@@ -8,10 +9,10 @@ if len(sys.argv) != 2:
 
 vcomp140_path = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\14.0\\VC\\redist\\x64\\Microsoft.VC140.OpenMP\\vcomp140.dll'
 
-wheel_path = sys.argv[1]
-m = re.search(r'xgboost-(.*)-py2.py3', wheel_path)
-assert m
-version = m.group(1)
+for wheel_path in sorted(glob.glob(sys.argv[1])):
+    m = re.search(r'xgboost-(.*)-py2.py3', wheel_path)
+    assert m
+    version = m.group(1)
 
-with zipfile.ZipFile(wheel_path, 'a') as f:
-    f.write(vcomp140_path, 'xgboost-{}.data/data/xgboost/vcomp140.dll'.format(version))
+    with zipfile.ZipFile(wheel_path, 'a') as f:
+        f.write(vcomp140_path, 'xgboost-{}.data/data/xgboost/vcomp140.dll'.format(version))
